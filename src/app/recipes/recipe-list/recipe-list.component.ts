bca8357d11efd0e92818383721cb99aa743dcd8f
yanisa.p <yanisa.p@gofive.co.th>
2022-11-04 15:48:44 +0700

    create model for recipe in recipe.model generate test data for recipes in recipe list create detailed structural for recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test description for Test Recipe',
+      'https://tse2.mm.bing.net/th?id=OIP.tDpTb_hQ41YG7yFw7tYfGwHaLH&pid=Api&P=0'
+    ),
+    new Recipe(
+      'Test Recipe2',
+      'Test description for Test Recipe2',
+      'https://i1.wp.com/www.frugelegance.com/wp-content/uploads/2015/04/HumBirdCakeBundt6.jpg'
+    ),
+  ];
+  // type Recipe[] menas array of Class Recipe, which was import from src\app\recipes\recipe.model.ts as declare above
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
