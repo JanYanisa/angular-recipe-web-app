b58b101c71a6cd0b126a002bbc307b9150ffc7ca
yanisa.p <yanisa.p@gofive.co.th>
2022-11-28 11:05:07 +0700

    manage recipes in recipe.service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,21 +11,13 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeSelectedRl = new EventEmitter<Recipe>();
   // type Recipe[] means array of Class Recipe, which was import from src\app\recipes\recipe.model.ts as declare above
   recipes: Recipe[] = [
-    new Recipe(
-      'Test Recipe',
-      'Test description for Test Recipe',
-      'https://tse2.mm.bing.net/th?id=OIP.tDpTb_hQ41YG7yFw7tYfGwHaLH&pid=Api&P=0'
-    ),
-    new Recipe(
-      'Test Recipe2',
-      'Test description for Test Recipe2',
-      'https://i1.wp.com/www.frugelegance.com/wp-content/uploads/2015/04/HumBirdCakeBundt6.jpg'
-    ),
   ];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes()
+  }
   onSelected(recipe: Recipe) {
     this.recipeSelectedRl.emit(recipe);
   }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,21 @@
+import { Recipe } from "./recipe.model";
 export class RecipeService {
-    
+    // type Recipe[] means array of Class Recipe, which was import from src\app\recipes\recipe.model.ts as declare above
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test description for Test Recipe',
+      'https://tse2.mm.bing.net/th?id=OIP.tDpTb_hQ41YG7yFw7tYfGwHaLH&pid=Api&P=0'
+    ),
+    new Recipe(
+      'Test Recipe2',
+      'Test description for Test Recipe2',
+      'https://i1.wp.com/www.frugelegance.com/wp-content/uploads/2015/04/HumBirdCakeBundt6.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    // return new copy of this.recipes, so no direct access 
+    return this.recipes.slice()
+  }
 }
