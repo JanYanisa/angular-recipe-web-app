9b5b9028ed0998e340ec0865a3061714dd436570
yanisa.p <yanisa.p@gofive.co.th>
2022-11-28 12:25:01 +0700

    Add ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredients } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -6,14 +7,19 @@ export class RecipeService {
     // type Recipe[] means array of Class Recipe, which was import from src\app\recipes\recipe.model.ts as declare above
     private recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe',
-      'Test description for Test Recipe',
-      'https://tse2.mm.bing.net/th?id=OIP.tDpTb_hQ41YG7yFw7tYfGwHaLH&pid=Api&P=0'
+      'Banana cake loaf',
+      'Banana cake loaf description for Banana cake loaf',
+      'https://tse2.mm.bing.net/th?id=OIP.tDpTb_hQ41YG7yFw7tYfGwHaLH&pid=Api&P=0',
+      [new Ingredients('Flour', 5),
+        new Ingredients('Sugar', 6),
+        new Ingredients('Nuts', 20)]
     ),
     new Recipe(
       'Test Recipe2',
       'Test description for Test Recipe2',
-      'https://i1.wp.com/www.frugelegance.com/wp-content/uploads/2015/04/HumBirdCakeBundt6.jpg'
+      'https://i1.wp.com/www.frugelegance.com/wp-content/uploads/2015/04/HumBirdCakeBundt6.jpg',
+      [new Ingredients('Cream', 10),
+        new Ingredients('Sugar',6)]
     ),
   ];
 
